e64cbb3811115e86b7b177be2e499d7145f23661
Mmoravec1118 <marty.kylar@gmail.com>
2017-02-05 09:51:46 -0700

    Built a cottage
    cottage

---
+++ DungeonCrawler/Assets/Scripts/Snap.cs
@@ -0,0 +1,24 @@
+﻿using Assets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Snap : MonoBehaviour {
+    public Constants.SnapSide snapSide;
+    bool locked = false;
+    Snap connectedTo;
+    public bool Locked
+    {
+        get { return locked; }
+        set { locked = value; }
+    }
+
+    public Snap ConnectedTo
+    {
+        get { return connectedTo; }
+        set { connectedTo = value; }
+    }
+	// Use this for initialization
+	
+}
