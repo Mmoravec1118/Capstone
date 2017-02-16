d8efde2a956c31aedb4c534376e6e48d4fd0dc4c
Mmoravec1118 <marty.kylar@gmail.com>
2017-02-16 13:29:01 -0700

    rectile
    rectile

--- Peasant/Assets/StoneKeep/Scripts/Interaction.js
+++ Peasant/Assets/StoneKeep/Scripts/Interaction.js
@@ -2,7 +2,7 @@
 var hit : RaycastHit;
 
 function Update () {
-	if(Physics.Raycast(transform.position, transform.forward, hit, 3)) {
+	if(Physics.Raycast(transform.position, transform.forward, hit, 5)) {
 		if(Input.GetMouseButtonDown(0)) {
 			if(hit.transform.tag == "Interact") {
 				hit.transform.SendMessage("Action", 0.0f);
